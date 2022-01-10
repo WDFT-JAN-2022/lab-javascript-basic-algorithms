0e0c92c3a324f4a2b234b2d45b8fd48357b20d5f
Eliza <seeubanks12@gmail.com>
2022-01-10 16:35:22 -0500

    Eliza Eubanks

--- js/index.js
+++ js/index.js
@@ -3,7 +3,7 @@
 let hacker1 = "Nicholas"
 let hacker2 = "Bob"
 console.log(`The driver's name is ${hacker1}`)
-
+console.log(`The navigator's name is ${hacker2}`)
 // Iteration 2: Conditionals
 
 // Iteration 3: Loops
