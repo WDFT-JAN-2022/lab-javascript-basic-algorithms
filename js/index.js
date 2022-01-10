51b35476720ac971c83b945090a5e218adb92e82
unknown <jeremylesser961508@gmail.com>
2022-01-10 16:56:26 -0500

    Jeremy Lesser

--- js/index.js
+++ js/index.js
@@ -5,5 +5,12 @@ let hacker2 = "Bob"
 console.log(`The driver's name is ${hacker1}`)
 console.log(`The navigator's name is ${hacker2}`)
 // Iteration 2: Conditionals
+if(hacker1.length > hacker2.length){
+console.log(`the driver has the longest name, it has ${hacker1.length} characters.`)
+}else if (hacker2.length > hacker2.length){
+    console.log(`the navigator has the longest name, it has ${hacker2.length}`)
+}else if (hacker1.length === hacker2.length){
+    console.log(`they are the same`)
+}
 
 // Iteration 3: Loops
